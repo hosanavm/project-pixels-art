f4cbf3368c26c4c8e704543d03582e8b34973220
Hosana <hosanavms@gmail.com>
2023-04-28 16:05:40 -0300

    adiciona função para pintar quadro com a cor selecionada

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ corPaletaThree.addEventListener('click', escolheCor); // evento que seleciona a 
 
 const corPaletaFour = document.querySelector('#colorFour');
 corPaletaFour.addEventListener('click', escolheCor); // evento que seleciona a cor ao clicar
+
+function pintar() {
+    const pixels = document.querySelectorAll('.pixel');// pega todos os elementos com a classe pixel
+    for (let index = 0; index < pixels.length; index += 1) { // percorre os elementos
+      const pixelEscolhido = pixels[index];// armazena os pixels
+      pixelEscolhido.addEventListener('click', () => { // evento que pinta o pixel
+        const corEscolhida = document.querySelector('.selected');// pega a cor com a classe selected
+        const cssCor = window.getComputedStyle(corEscolhida);// busca o css do elemento buscado
+        const corUsada = cssCor.getPropertyValue('background');// pega a propriedade background
+        event.target.style.background = corUsada;// leva o background para o pixel escolhido
+      });
+    }
+  }
+  pintar();// chama a função
+  // .getComputedStyle encontrado em https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
